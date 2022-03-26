542da21484aa1d5be96acab78dfcbe78bb02e3cd
Sima612 <sima101531@yahoo.com>
2022-03-26 12:26:05 -0700

    added favicon

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,10 +4,11 @@ function Default(html) {
     return (
         <html>
             <head>
-                <title>{html.title || 'Default'}</title>
+                <title>{html.title || 'My Bread Page'}</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css" integrity="sha512-EZLkOqwILORob+p0BXZc+Vm3RgJBOe1Iq/0fiI7r/wJgzOFZMlsqTa29UEl6v6U6gsV4uIpsNZoV32YZqrCRCQ==" crossOrigin="anonymous" />
                 <link rel="stylesheet" href="/main.css"></link>
+                <link rel="shortcut icon" type="image/jpg" href="../images/bread-icon.png"/>
                 </head>
             <body>
                 <div className='wrapper'>
