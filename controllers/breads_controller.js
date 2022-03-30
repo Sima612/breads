814d6f345977c1ba0e52f67ceb610bd2810af4cb
Sima612 <sima101531@yahoo.com>
2022-03-30 00:40:40 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -28,6 +28,14 @@ breads.delete('/:indexArray', (req, res) => {
     res.status(303).redirect('/breads')
 })
 
+// EDIT BREAD
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
